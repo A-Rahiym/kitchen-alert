292def7ca9aea0d8d24b095b4814285d7bba1085
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-07-17 19:58:06 +0100

    chore: removed shadow on tab nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -28,7 +28,6 @@ export default function TabLayout() {
         activeColor={colors.tabActive}
         inactiveColor={colors.tabInactive}
         tabBarStyle={{
-          // shadowColor: "#9CA3AF",
           shadowOffset: { width: 0, height: -2 },
           shadowOpacity: 0.05,
           shadowRadius: 8,
