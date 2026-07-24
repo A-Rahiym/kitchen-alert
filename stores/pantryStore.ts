c44bf228def81adee5909674a021a9aca173438a
Abdulrahman Abdulrahim <a.raheeem0001@gmail.com>
2026-07-24 23:27:06 +0100

    feat: add unit, consumptionUnit, conversionRate to PantryItem interface and mock data

--- stores/pantryStore.ts
+++ stores/pantryStore.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 
+export type ConsumptionUnit = "teaspoon" | "tablespoon" | "cup" | "piece" | "ml" | "minute";
+
 export interface PantryItem {
   id: string;
   name: string;
@@ -8,6 +10,9 @@ export interface PantryItem {
   icon: string;
   frequency: number;
   people: number;
+  unit: string;
+  consumptionUnit: ConsumptionUnit;
+  conversionRate: number;
 }
 
 interface PantryState {
@@ -18,12 +23,12 @@ interface PantryState {
 }
 
 const mockItems: PantryItem[] = [
-  { id: "p1", name: "Cooking Gas", itemSize: "12.5kg", thresholdDays: 7, icon: "gas", frequency: 3, people: 4 },
-  { id: "p2", name: "Eggs", itemSize: "30pk", thresholdDays: 3, icon: "egg", frequency: 2, people: 3 },
-  { id: "p3", name: "Rice", itemSize: "50kg", thresholdDays: 14, icon: "rice", frequency: 3, people: 4 },
-  { id: "p4", name: "Flour", itemSize: "25kg", thresholdDays: 10, icon: "flour", frequency: 2, people: 4 },
-  { id: "p5", name: "Vegetable Oil", itemSize: "5L", thresholdDays: 14, icon: "oil", frequency: 3, people: 4 },
-  { id: "p6", name: "Kerosene", itemSize: "4L", thresholdDays: 5, icon: "kerosene", frequency: 2, people: 3 },
+  { id: "p1", name: "Cooking Gas", itemSize: "12.5kg", thresholdDays: 7, icon: "gas", frequency: 3, people: 4, unit: "KG", consumptionUnit: "minute", conversionRate: 120 },
+  { id: "p2", name: "Eggs", itemSize: "30pk", thresholdDays: 3, icon: "egg", frequency: 2, people: 3, unit: "PACK", consumptionUnit: "piece", conversionRate: 30 },
+  { id: "p3", name: "Rice", itemSize: "50kg", thresholdDays: 14, icon: "rice", frequency: 3, people: 4, unit: "KG", consumptionUnit: "cup", conversionRate: 5 },
+  { id: "p4", name: "Flour", itemSize: "25kg", thresholdDays: 10, icon: "flour", frequency: 2, people: 4, unit: "KG", consumptionUnit: "cup", conversionRate: 8 },
+  { id: "p5", name: "Vegetable Oil", itemSize: "5L", thresholdDays: 14, icon: "oil", frequency: 3, people: 4, unit: "L", consumptionUnit: "ml", conversionRate: 1000 },
+  { id: "p6", name: "Kerosene", itemSize: "4L", thresholdDays: 5, icon: "kerosene", frequency: 2, people: 3, unit: "L", consumptionUnit: "minute", conversionRate: 180 },
 ];
 
 export const usePantryStore = create<PantryState>((set) => ({
